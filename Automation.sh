2d6df1ba772e2af83e9bd78c49513b880986372c
root <root@ip-172-31-30-102.ec2.internal>
2022-02-18 10:57:14 +0000

    script1

---
+++ Automation.sh
@@ -0,0 +1,31 @@
+s3bucket="upgrad-vijay"
+name="vijay"
+
+apt update -y
+
+if [[ apache2 != $(dpkg --get-selections apache2 | awk '{print $1}')]];
+then
+    apt install apache2 -y
+fi
+
+running=$(systemctl status apache2 | grep active | awk '{print $3}' | tr -d'()')
+if [[running != ${running}]]; 
+then
+    systemctl start apache2
+fi
+enabled=$(systemctl is -enabled apache2 | grep "enabled")
+if [[ enabled != ${enabled}]];
+then
+    systemctl enable apache2
+fi
+timestamp = $(date '+%d%m%Y-%H%M%S')
+
+cd /var/log/apache2
+tar -cf /temp/${name}-httpd-logs-${timestamp}.tar *.log
+
+if [[ -f /tmp/${name}-httpd-logs-${timestamp}.tar ]];
+then 
+    aws cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3bucket}/${name}-httpd-logs-${timestamp}.tar
+fi
+
+	
